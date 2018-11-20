364ab351c6a43c52282241ce219671a03579c3d6
lukechadwick <llukechadwick@gmail.com>
2018-11-21 12:33:47 +1300

    article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Article < ActiveRecord::Base
+  validates :title, presense: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presense: true, length: { minimum: 5, maximum: 300 }
 end
