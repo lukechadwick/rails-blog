ddd8738d8a18c0f09da8c513bfd9f0fda6f32a2b
lukechadwick <llukechadwick@gmail.com>
2018-11-21 13:02:11 +1300

    ApplicationRecord for rails 5+

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Article < ActiveRecord::Base
-  validates :title, presense: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presense: true, length: { minimum: 5, maximum: 300 }
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 300 }
 end
