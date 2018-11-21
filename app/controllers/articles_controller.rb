4faad7ff861a2441c4d42a3980f72ddef58eb9d6
lukechadwick <llukechadwick@gmail.com>
2018-11-21 16:07:03 +1300

    show errors in html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,9 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
 
-    if article.save
+    if @article.save
       flash[:notice] = 'Article was successfully created'
-      redirect_to articles_show(@article)
+      redirect_to article_path(@article)
     else
       render 'new'
     end
